a806e62e05b69260ef1f0cc13042fcb548b5c547
nhi-ngo <nhingo.y@gmail.com>
2020-04-21 22:20:38 -0400

    13 - Understanding State

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -13,7 +13,7 @@ class StorePicker extends React.Component {
     // 1. Stop the form from submitting
     event.preventDefault();
     // 2. Get the text from that input
-    const storeName = this.myInput.value.value;
+    const storeName = this.myInput.current.value;
     // 3. Change the page to /store/whatever-they-entered
     this.props.history.push(`/store/${storeName}`);
   };
@@ -22,13 +22,7 @@ class StorePicker extends React.Component {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input
-          type="text"
-          ref={this.myInput}
-          required
-          placeholder="Store Name"
-          defaultValue={getFunName()}
-        />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store -></button>
       </form>
     );
