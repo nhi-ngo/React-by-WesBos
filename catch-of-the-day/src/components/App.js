271d4824a9030e690fd2a926589669e47fec60ca
nhi-ngo <nhingo.y@gmail.com>
2020-04-23 14:06:45 -0400

    19 - Persisting our State with localStorage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,11 +13,25 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+    // When the component mounts, we are updating state which
+    // in turn triggering componentDidUpdate()
+
     const { params } = this.props.match;
+    // First reinstate our local storage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+    
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes',
     });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.order);
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
   }
 
   componentWillUnmount() {
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,7 +5,10 @@ class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    // Make sure the fish is loaded before we continue
+    if (!fish) return null;
+
     if (!isAvailable) {
       return (
         <li key={key}>
@@ -42,7 +45,7 @@ class Order extends React.Component {
           Total: <strong> {formatPrice(total)} </strong>
         </div>
       </div>
-    );  
+    );
   }
 }
 
